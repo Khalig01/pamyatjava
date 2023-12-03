fefd3b0bbd7f953ad42c8e9c3df254f0f91df1b1
Khal01 <aliyev_56@list.ru>
2023-12-03 14:42:54 +0300

    pamyat sdelano

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,30 @@ public class Main {
         System.out.println(value2);
         Integer[] array= new Integer[] {3,4};
         System.out.println(Arrays.toString(array));
+        changeArrayValue(array);
+        System.out.println(Arrays.toString(array));
+        Person p= new Person("Lyapis", "Trubetskoy");
+        p.changePerson(p);
+        System.out.println(p);
+        p.changePerson2(p);
+        System.out.println(p);
+
     }
     private static void changeValue(int value) {
+
         value = 22;
     }
     private static void changeValue(Integer value) {
+
         value = 22;
     }
     private static void changeValue(Integer[] arr) {
-        arr= new Integer[] {1,2};
 
-
+        arr = new Integer[]{1, 2};
+    }
+    private static void changeArrayValue(Integer[] arr) {
+        arr[0] = 99;
+    }
 
 
     }
-}
